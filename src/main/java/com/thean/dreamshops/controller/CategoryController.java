d0e948bbd72cca46d9092f744c68a7519d482e61
thean <annguyenthe2003@gmail.com>
2025-02-07 22:44:54 +0700

    commit 07/02/2025

--- src/main/java/com/thean/dreamshops/controller/CategoryController.java
+++ src/main/java/com/thean/dreamshops/controller/CategoryController.java
@@ -1,4 +1,82 @@
 package com.thean.dreamshops.controller;
 
+import com.thean.dreamshops.dto.CategoryDTO;
+import com.thean.dreamshops.exception.AlredyExistingException;
+import com.thean.dreamshops.exception.NotFoundException;
+import com.thean.dreamshops.model.Category;
+import com.thean.dreamshops.response.ApiResponse;
+import com.thean.dreamshops.service.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found!", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody CategoryDTO categoryDTO) {
+        try {
+            Category category = categoryService.addCategory(categoryDTO);
+            return ResponseEntity.ok(new ApiResponse("Added Category", category));
+        } catch (AlredyExistingException e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id) {
+        try {
+            Category category = categoryService.getCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Found!", category));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/category/{name}")
+    public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name) {
+        try {
+            Category category = categoryService.getCategoryByName(name);
+            return ResponseEntity.ok(new ApiResponse("Found!", category));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id) {
+        try {
+            categoryService.deleteCategory(id);
+            return ResponseEntity.ok(new ApiResponse("Found!", null));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody CategoryDTO categoryDTO) {
+        try {
+            Category updatedCategory = categoryService.updateCategory(categoryDTO,id);
+            return ResponseEntity.ok(new ApiResponse("Updated success!", updatedCategory));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
--- src/main/java/com/thean/dreamshops/controller/ProductController.java
+++ src/main/java/com/thean/dreamshops/controller/ProductController.java
@@ -1,4 +1,73 @@
 package com.thean.dreamshops.controller;
 
+import com.thean.dreamshops.dto.ProductDTO;
+import com.thean.dreamshops.exception.NotFoundException;
+import com.thean.dreamshops.model.Product;
+import com.thean.dreamshops.response.ApiResponse;
+import com.thean.dreamshops.service.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import static org.springframework.http.HttpStatus.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+    private final IProductService productService;
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(new ApiResponse("success", products));
+    }
+    @GetMapping("/product/{id}")
+    public ResponseEntity<ApiResponse> getProductById(@PathVariable Long id) {
+        try {
+            Product product = productService.getProductById(id);
+            return ResponseEntity.ok(new ApiResponse("success", product));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody ProductDTO productDTO) {
+        try {
+            Product product = productService.addProduct(productDTO);
+            return ResponseEntity.ok(new ApiResponse("Add product success", product));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+    @PutMapping("/update/{productId}")
+    public ResponseEntity<ApiResponse> updateProduct(@RequestBody ProductDTO productDTO, @PathVariable Long productId) {
+        try {
+            Product product = productService.updateProduct(productDTO, productId);
+            return ResponseEntity.ok(new ApiResponse("Update product success!", product));
+        } catch (NotFoundException e) {
+           return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+    @DeleteMapping("/delete/{productId}")
+    public ResponseEntity<ApiResponse> deleteProduct(@PathVariable Long productId) {
+        try {
+            productService.deleteProductById(productId);
+            return ResponseEntity.ok(new ApiResponse("Delete product success!", productId));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+@GetMapping("/by/brand-and-name")
+    public ResponseEntity<ApiResponse> getProductByBrandAndName(@RequestParam String brand, @RequestParam String name) {
+        try {
+            List<Product> products = productService.getProductsByBrandAndName(brand, name);
+            if (products.isEmpty()) {
+                return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("No products found", null));
+            }
+            return ResponseEntity.ok(new ApiResponse("success", products));
+        } catch (Exception e) {
+           return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
 }
